2b992acc8d33fad8a73e0004bb17aeab0f61ba9f
RCDMK <rcdmk@hotmail.com>
2019-07-21 14:59:36 -0300

    Update config pacakge with additional DB options

--- infra/config/mapping.go
+++ infra/config/mapping.go
@@ -1,5 +1,7 @@
 package config
 
+import "time"
+
 // Config holds configuration values for the entire application
 type Config struct {
 	DB DBConfig `mapstructure:"db"`
@@ -9,9 +11,12 @@ type Config struct {
 // In a production scenario the secrets would be loaded from AWS SSM
 // and the config files would only have the secret keys instead.
 type DBConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	Schema   string `mapstructure:"schema"`
-	User     string `mapstructure:"user"`
-	Password string `mapstructure:"password"`
+	Host         string        `mapstructure:"host"`
+	Port         int           `mapstructure:"port"`
+	Schema       string        `mapstructure:"schema"`
+	User         string        `mapstructure:"user"`
+	Password     string        `mapstructure:"password"`
+	MaxConnLife  time.Duration `mapstructure:"max-conn-life"`
+	MaxIdleConns int           `mapstructure:"max-idle-conns"`
+	MaxOpenConns int           `mapstructure:"max-open-conns"`
 }

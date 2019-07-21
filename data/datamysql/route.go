ce5e02e7694b4b2a76136e18a69b4d5a47c619a0
RCDMK <rcdmk@hotmail.com>
2019-07-21 14:40:02 -0300

    Implement routes repo

---
+++ data/datamysql/route.go
@@ -0,0 +1,55 @@
+package datamysql
+
+import (
+	"github.com/rcdmk/shortest-flight-path/domain/entity"
+)
+
+// routeRepo holds methods to fetch route data
+type routeRepo struct {
+	conn executor
+}
+
+// newRouteRepo returns a new route repo instance
+func newRouteRepo(conn executor) *routeRepo {
+	return &routeRepo{
+		conn: conn,
+	}
+}
+
+// GetByID returns an route instance with the given two letter code
+func (repo *routeRepo) GetAllDepartingFromAirport(airportIATA3 string) (routes []entity.Route, err error) {
+	const query = `
+		SELECT 		r.airline_code
+					r.origin,
+					r.destination
+		
+		FROM 		tb_route 	r
+
+		WHERE 		r.origin 	= 	?
+		
+		LIMIT 1;
+	`
+
+	rows, err := repo.conn.Query(query, airportIATA3)
+	if err != nil {
+		return routes, parseError(err)
+	}
+
+	routes = make([]entity.Route, 0)
+	var route entity.Route
+
+	for rows.Next() {
+		err = rows.Scan(
+			&route.AirlineCode,
+			&route.Origin,
+			&route.Destination,
+		)
+		if err != nil {
+			return routes, parseError(err)
+		}
+
+		routes = append(routes, route)
+	}
+
+	return routes, nil
+}

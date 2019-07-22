ebc4e1b438f0b74efab35f1f6afc02f97a2c189c
RCDMK <rcdmk@hotmail.com>
2019-07-22 01:49:08 -0300

    Add mock capabiities to mock DataManager

--- data/datamock/datamock.go
+++ data/datamock/datamock.go
@@ -2,6 +2,7 @@ package datamock
 
 import (
 	"github.com/rcdmk/shortest-flight-path/domain/contract"
+	"github.com/stretchr/testify/mock"
 )
 
 // New returns a mocked data manager implementation
@@ -15,22 +16,33 @@ func New() *DataManager {
 
 // DataManager is the mocked data manager implementation
 type DataManager struct {
+	mock.Mock
+
 	airlines *AirlineRepo
 	airports *AirportRepo
 	routes   *RouteRepo
 }
 
+// Close simlates closing the connection
+func (dm *DataManager) Close() error {
+	args := dm.Called()
+	return args.Error(0)
+}
+
 // Airlines returns an airlines repo instance
 func (dm *DataManager) Airlines() contract.AirlineRepo {
+	dm.Called()
 	return dm.airlines
 }
 
 // Airports returns an airport repo instance
 func (dm *DataManager) Airports() contract.AirportRepo {
+	dm.Called()
 	return dm.airports
 }
 
 // Routes returns a route repo instance
 func (dm *DataManager) Routes() contract.RouteRepo {
+	dm.Called()
 	return dm.routes
 }

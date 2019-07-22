1ab5657b40cb809386b4c3f0066b50bc36e3bedc
RCDMK <rcdmk@hotmail.com>
2019-07-22 01:50:42 -0300

    Update errors pacakge with new error types

--- infra/errors/error.go
+++ infra/errors/error.go
@@ -12,3 +12,35 @@ const (
 	// NotFound represents mising data error
 	NotFound errorString = "not found"
 )
+
+// ValidationError represents a input validation error
+type ValidationError struct {
+	Message string
+}
+
+func (e *ValidationError) Error() string {
+	return e.Message
+}
+
+// NewValidationError returns a new ValidationError
+func NewValidationError(message string) error {
+	return &ValidationError{
+		Message: message,
+	}
+}
+
+// NotFoundError represents a data not found error
+type NotFoundError struct {
+	Message string
+}
+
+func (e *NotFoundError) Error() string {
+	return e.Message
+}
+
+// NewNotFoundError returns a new NotFoundError
+func NewNotFoundError(message string) error {
+	return &NotFoundError{
+		Message: message,
+	}
+}

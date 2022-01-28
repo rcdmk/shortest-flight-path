3a05e340e160b72a676bc289c3406478a810747e
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-01-27 21:56:43 -0800

    Add cache configuration

--- infra/config/mapping.go
+++ infra/config/mapping.go
@@ -5,6 +5,7 @@ import "time"
 // Config holds configuration values for the entire application
 type Config struct {
 	DB     DBConfig     `mapstructure:"db"`
+	Cache  CacheConfig  `mapstructure:"cache"`
 	Server ServerConfig `mapstructure:"server"`
 }
 
@@ -20,6 +21,21 @@ type DBConfig struct {
 	MaxConnLife  time.Duration `mapstructure:"max-conn-life"`
 	MaxIdleConns int           `mapstructure:"max-idle-conns"`
 	MaxOpenConns int           `mapstructure:"max-open-conns"`
+}
+
+// CacheConfig holds ache related configuration options
+// In a production scenario the secrets would be loaded from AWS SSM
+// and the config files would only have the secret keys instead.
+type CacheConfig struct {
+	Host                  string        `mapstructure:"host"`
+	Port                  int           `mapstructure:"port"`
+	DBNumber              int           `mapstructure:"db-number"`
+	Password              string        `mapstructure:"password"`
+	DefaultExpirationTime time.Duration `mapstructure:"default-expiration-time"`
+	MaxConnLife           time.Duration `mapstructure:"max-conn-life"`
+	MaxOpenConns          int           `mapstructure:"max-open-conns"`
+	MinIdleConns          int           `mapstructure:"min-idle-conns"`
+	Prefix                string        `mapstructure:"prefix"`
 }
 
 // ServerConfig holds API server configuration options
